545fdfd9634bf42aff813250b9acf2c8c521f7ad
IstodeVlad <68825785+IstodeVlad@users.noreply.github.com>
2020-10-15 12:36:13 +0300

    Add files via upload

---
+++ src/components/Browser.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { object } from 'prop-types';
+import Web3 from 'web3';
+import KittyCoreABI from '../contracts/KittyCoreABI.json';
+import { CONTRACT_NAME, CONTRACT_ADDRESS } from '../config';
+
+class Browser extends Component {
+  
+  componentDidMount() {
+    const web3 = new Web3(window.web3.currentProvider);
+
+    // Initialize the contract instance
+
+    const kittyContract = new web3.eth.Contract(
+      KittyCoreABI, // import the contracts's ABI and use it here
+      CONTRACT_ADDRESS,
+    );
+
+    // Add the contract to the drizzle store
+
+    this.context.drizzle.addContract({
+      contractName: CONTRACT_NAME,
+      web3Contract: kittyContract,
+    });
+  }
+
+  render() {
+    return (
+      <div className="browser">
+        <h1>
+          Kitty Browser
+        </h1>
+
+        {/* Input to type in the kitty ID here */}
+        <form>
+          <label>Kitty ID:</label><br></br>
+          <input type = 'text' id='kittyID' name='id'></input>
+          <input type = 'submit'></input>
+        </form>
+        <br></br>
+
+
+        {/* Display Kitty info here */}
+        
+        Genes:
+        <br></br><br></br>
+        Generation:
+        <br></br><br></br>
+        Birth Time:
+        <br></br><br></br>
+        
+        
+        
+      </div>
+    );
+  }
+}
+
+Browser.contextTypes = {
+  drizzle: object,
+};
+
+export default Browser;
